86c7ef89af65b63dc29c68ad6a05af5196f038ac
Marco Sotomayor <msotomayor@dc.uba.ar>
2021-08-02 13:46:53 -0300

    Añadido ButtonCallBackQuery que devuelve las respuestas de botones en el context, ya partidas

--- animexactasbot.py
+++ animexactasbot.py
@@ -11,7 +11,7 @@ from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
 # Local imports
 import models
 from errors import error_callback
-from handlers.button.button_handler import button_handler, te_doy_botones
+from handlers.button.button_handler import button_handler, te_doy_botones, ButtonCallbackQueryHandler
 from handlers.ejemplo.dame_botones import dame_botones
 from handlers.polls.create_poll import create_poll
 from handlers.polls.sugerir_opcion import (
@@ -103,7 +103,7 @@ def main():
         votar_handler = CommandHandler('votar', votar)
         dispatcher.add_handler(votar_handler)
 
-        te_doy_botones_handler = CallbackQueryHandler(
+        te_doy_botones_handler = ButtonCallbackQueryHandler(
             te_doy_botones,
             run_async=True,
             pattern='^' + "dame_botones"
--- handlers/button/button_handler.py
+++ handlers/button/button_handler.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+from typing import Union
+
 from telegram import InlineKeyboardMarkup, Update
+from telegram.ext import CallbackQueryHandler, Dispatcher
+from telegram.ext.handler import UT, RT
+from telegram.ext.utils.types import CCT
+from telegram.ext.utils.promise import Promise
+
 from telegram.ext.callbackcontext import CallbackContext
+import logging
+
+logger = logging.getLogger('animexactasbot.log')
+
+class ButtonCallbackQueryHandler(CallbackQueryHandler):
+    def handle_update(
+        self,
+        update: UT,
+        dispatcher: 'Dispatcher',
+        check_result: object,
+        context: CCT = None,
+    ) -> Union[RT, Promise]:
+        query = update.callback_query
+        callback_arguments = query.data.split("|")
+        for i in range(1, len(callback_arguments)):
+            context.user_data[f"button_reply_{i}"] = callback_arguments[i]
+
+        return super().handle_update(
+            update,
+            dispatcher,
+            check_result,
+            context
+        )
+        
 
 # noinspection Pylint
 """ 
@@ -42,8 +74,9 @@ def button_handler(update: Update, context: CallbackContext) -> None:
 
 def te_doy_botones(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
-    callback_arguments = query.data.split("|")
-    coso = callback_arguments[1]
+    # callback_arguments = query.data.split("|")
+    # coso = callback_arguments[1]
+    coso = context.user_data["button_reply_1"]
     query.message.reply_text(f"apretaste algún botón y ahora se que sos un {coso}")
     try:
         query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup([]))
