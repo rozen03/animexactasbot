18069bd253bf351975d81e7c75c47593a739cf28
Rozen <rozenberguriel@gmail.com>
2021-09-27 11:36:21 -0300

    Listar (#32)
    * Función para inicializar una base de datos de cero (para testing)
    
    * fix pep8 things
    
    * Comando votar: Callbacks + Guardado de Vote con user genérico
    
    * Pylint complacency
    
    * Pylint complacency II
    
    * removed repeated e in bot name xD
    
    * Url embebidos en las opciones
    
    * Funciones utils + Query con poll id
    
    * Pylint complacency III
    
    * Pylint always watches
    
    * Pylint lurking
    
    * Comando listar (no responde a los botones aún)
    
    * fix handler not handling and edit messages
    
    * sort by sql not by code
    
    * Listado limitado a 100 hipervínculos + Múltiples mensajes
    
    * Listar con ids + Comando editopt sin validación al final
    
    * Fix draw_margin -> drawMargin
    
    Co-authored-by: Facundo <facundoruiz2610@gmail.com>

--- usecases/polls/listar.py
+++ usecases/polls/listar.py
@@ -5,5 +5,6 @@ from models import Option, db_session, select
 
 @db_session
 def list_poll_options(poll_id):
-    option_list = select((o.text, o.url) for o in Option if o.poll.id == poll_id).order_by(lambda: o.text)[:]
+    option_list = select((o.text, o.url, o.id) for o in Option if o.poll.id == poll_id).order_by(lambda: o.text)[:]
+
     return option_list
